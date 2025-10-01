d80ebf86aa582621cacea09165ebed38ed331fc7
kajol <kajolchauhan450@gmail.com>
2025-10-01 18:03:04 +0530

    fourth

--- Frontend/vite-project/src/utils/constant.js
+++ Frontend/vite-project/src/utils/constant.js
@@ -1,5 +1,5 @@
 export const USER_API_END_POINT="https://job-portal-3-w2ra.onrender.com/api/v1/user";
-export const Job_API_END_POINT="https://job-portal-3-w2ra.onrender.com/v1/job"
+export const Job_API_END_POINT="https://job-portal-3-w2ra.onrender.com/api/v1/job"
 export const Application_API_END_POINT="https://job-portal-3-w2ra.onrender.com/api/v1/application"
 export const Company_API_END_POINT="https://job-portal-3-w2ra.onrender.com/api/v1/company"
 
